5449308c95c380bfdbb3ccefa1a84261574249a4
NovaVoidHowl <me@novavoidhowl.com>
2024-01-07 00:23:16 +0000

    fix(DSU): added warning on UI load issues

--- Runtime/CVRFury/Components/CVRFuryDataStorageUnit.cs
+++ Runtime/CVRFury/Components/CVRFuryDataStorageUnit.cs
@@ -8,5 +8,13 @@ namespace uk.novavoidhowl.dev.cvrfury.runtime
   ///
   /// </summary>
   [AddComponentMenu("CVRFury Data Storage Unit")]
-  public class CVRFuryDataStorageUnit : MonoBehaviour { }
+  public class CVRFuryDataStorageUnit : MonoBehaviour
+  {
+    // label to Say that the DSU has not correctly loaded and
+    // to check the Tool Setup, this is a bit of a hack to let the user
+    // know that something is wrong, without needing
+    // unityEditor in the runtime
+    [Header("CVRFury UI failed to load\n\nPlease check Tool Setup\n\n NVH>CVRFury>Tool Setup\n")]
+    public bool somethingIsBroken = true;
+  }
 }
