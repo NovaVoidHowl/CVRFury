02fe9efa2a344b232fd05ae34949c1e49b7e173a
NovaVoidHowl <me@novavoidhowl.com>
2024-01-07 00:19:46 +0000

    fix(app core): update Managed Folder

--- packagecore/Constants.cs
+++ packagecore/Constants.cs
@@ -8,7 +8,7 @@ namespace uk.novavoidhowl.dev.cvrfury.packagecore
     public const string PROGRAM_DISPLAY_NAME = "CVRFury";
     public const string SCRIPTING_DEFINE_SYMBOL = "NVH_CVRFURY_EXISTS";
     public const string PACKAGE_NAME = "uk.novavoidhowl.dev.cvrfury";
-    public const string ASSETS_MANAGED_FOLDER = "Assets/NovaVoidHowl/CVRFury";
+    public const string ASSETS_MANAGED_FOLDER = "Assets/_CVRFury";
 
     public static readonly Color UI_UPDATE_OUT_OF_DATE_COLOR = new Color(1.0f, 0f, 0f); // Red
     public static readonly Color UI_UPDATE_OUT_OF_DATE_COLOR_TEXT = new Color(1.0f, 0.4f, 0.4f); // Red
