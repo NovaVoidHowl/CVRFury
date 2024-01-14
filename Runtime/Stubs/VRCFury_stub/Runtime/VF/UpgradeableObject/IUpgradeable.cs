dcfb8ededd917593dd4438eb8b509b539ea03fb2
NovaVoidHowl <me@novavoidhowl.com>
2024-01-14 19:42:42 +0000

    fix(vrcfuryStub): add UpgradeableObject

---
+++ Runtime/Stubs/VRCFury_stub/Runtime/VF/UpgradeableObject/IUpgradeable.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace VF.Upgradeable
+{
+  public interface IUpgradeable : ISerializationCallbackReceiver
+  {
+    bool Upgrade(int fromVersion);
+    int GetLatestVersion();
+    int Version { get; set; }
+  }
+}
---
+++ Runtime/Stubs/VRCFury_stub/Runtime/VF/UpgradeableObject/VrcfUpgradeable.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+namespace VF.Upgradeable
+{
+  [Serializable]
+  public abstract class VrcfUpgradeable : IUpgradeable
+  {
+    [SerializeField]
+    private int version = -1;
+    public int Version
+    {
+      get => version;
+      set => version = value;
+    }
+
+    void ISerializationCallbackReceiver.OnAfterDeserialize()
+    {
+      this.IUpgradeableOnAfterDeserialize();
+    }
+
+    void ISerializationCallbackReceiver.OnBeforeSerialize()
+    {
+      PreSerialize();
+      this.IUpgradeableOnBeforeSerialize();
+    }
+
+    public virtual bool Upgrade(int fromVersion)
+    {
+      return false;
+    }
+
+    public virtual int GetLatestVersion()
+    {
+      return 0;
+    }
+
+    protected virtual void PreSerialize() { }
+  }
+}
