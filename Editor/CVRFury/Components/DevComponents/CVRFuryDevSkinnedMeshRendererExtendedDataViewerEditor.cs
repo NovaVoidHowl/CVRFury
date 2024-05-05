2395a49d286868fc8b5081a7df925c84da917515
NovaVoidHowl <me@novavoidhowl.com>
2024-05-05 20:06:21 +0100

    fix(SkinnedMeshRendererExtended): switch to pure UIElements (remove IMGUI dependency)

--- Editor/CVRFury/Components/DevComponents/CVRFuryDevSkinnedMeshRendererExtendedDataViewerEditor.cs
+++ Editor/CVRFury/Components/DevComponents/CVRFuryDevSkinnedMeshRendererExtendedDataViewerEditor.cs
@@ -1,6 +1,7 @@
 //#if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
@@ -16,6 +17,7 @@ namespace uk.novavoidhowl.dev.cvrfury.editor.components
   {
     // refresh button
     private Button _refreshButton;
+
     // base visual element of the component
     private VisualElement _baseVisualElement;
 
@@ -23,9 +25,9 @@ namespace uk.novavoidhowl.dev.cvrfury.editor.components
     private SkinnedMeshRenderer _skinnedMeshRenderer = null;
 
     // path to the USS stylesheet
-    private readonly string _pathToStyleSheet = Constants.PROGRAM_DISPLAY_NAME + "/CVRFuryComponents/UnityStyleSheets/CVRFuryDevSkinnedMeshRendererExtendedDataViewer";
-
-
+    private readonly string _pathToStyleSheet =
+      Constants.PROGRAM_DISPLAY_NAME
+      + "/CVRFuryComponents/UnityStyleSheets/CVRFuryDevSkinnedMeshRendererExtendedDataViewer";
 
     public override VisualElement CreateInspectorGUI()
     {
@@ -44,27 +46,27 @@ namespace uk.novavoidhowl.dev.cvrfury.editor.components
       if (stylesheet == null)
       {
         CoreLogError(
-          "Failed to load StyleSheet at '" + _pathToStyleSheet + "'. Please ensure the file exists at the specified path."
+          "Failed to load StyleSheet at '"
+            + _pathToStyleSheet
+            + "'. Please ensure the file exists at the specified path."
         );
         // If the StyleSheet was not loaded add a new label to the root.
         _baseVisualElement.Add(new Label("CRITICAL ERROR : StyleSheet could not be loaded."));
       }
       // Apply the StyleSheet to the root VisualElement
       _baseVisualElement.styleSheets.Add(stylesheet);
-    
+
       return _baseVisualElement;
     }
 
     // when the refresh button is clicked
     private void OnRefreshButtonClicked()
     {
-    
       // clear the base visual element of the component
       _baseVisualElement.Clear();
 
       // set the name of the component to allow for styling
       _baseVisualElement.name = "CVRFuryDevSkinnedMeshRendererExtendedDataViewer";
-
 
       // Load and apply the stylesheet
       var stylesheet = Resources.Load<StyleSheet>(_pathToStyleSheet);
@@ -73,7 +75,9 @@ namespace uk.novavoidhowl.dev.cvrfury.editor.components
       if (stylesheet == null)
       {
         CoreLogError(
-          "Failed to load StyleSheet at '" + _pathToStyleSheet + "'. Please ensure the file exists at the specified path."
+          "Failed to load StyleSheet at '"
+            + _pathToStyleSheet
+            + "'. Please ensure the file exists at the specified path."
         );
         // If the StyleSheet was not loaded add a new label to the root.
         _baseVisualElement.Add(new Label("CRITICAL ERROR : StyleSheet could not be loaded."));
@@ -85,14 +89,16 @@ namespace uk.novavoidhowl.dev.cvrfury.editor.components
       _baseVisualElement.Add(_refreshButton);
 
       // get the skinned mesh renderer component
-      _skinnedMeshRenderer = ((CVRFuryDevSkinnedMeshRendererExtendedDataViewer)target).gameObject.GetComponent<SkinnedMeshRenderer>();
+      _skinnedMeshRenderer = (
+        (CVRFuryDevSkinnedMeshRendererExtendedDataViewer)target
+      ).gameObject.GetComponent<SkinnedMeshRenderer>();
 
       // if the skinned mesh renderer component is null
       if (_skinnedMeshRenderer == null)
       {
         // create a new message box visual element
         var messageBox = new Label("Skinned Mesh Renderer component not found");
-        
+
         // add the message box to the base visual element of the component
         _baseVisualElement.Add(messageBox);
       }
@@ -113,8 +119,10 @@ namespace uk.novavoidhowl.dev.cvrfury.editor.components
 
         // create a new foldout
         var foldout = new Foldout { value = ((CVRFuryDevSkinnedMeshRendererExtendedDataViewer)target).foldoutState };
-        foldout.RegisterValueChangedCallback(evt => ((CVRFuryDevSkinnedMeshRendererExtendedDataViewer)target).foldoutState = evt.newValue);
-        
+        foldout.RegisterValueChangedCallback(
+          evt => ((CVRFuryDevSkinnedMeshRendererExtendedDataViewer)target).foldoutState = evt.newValue
+        );
+
         // add the foldout to the extended data visual element
         extendedDataVisualElement.Add(foldout);
 
@@ -128,7 +136,8 @@ namespace uk.novavoidhowl.dev.cvrfury.editor.components
         var container = new VisualElement();
         // set the class of the container
         container.AddToClassList("container");
-        
+
+        #region Mesh reference
         // create a new label
         var meshLabel = new Label("Mesh");
         container.Add(meshLabel);
@@ -170,38 +179,40 @@ namespace uk.novavoidhowl.dev.cvrfury.editor.components
 
         // add the container to the content
         content.Add(container);
+        #endregion
 
+        #region Root Bone reference
         // create a new container for the root bone
         var rootBoneContainer = new VisualElement();
         rootBoneContainer.AddToClassList("container");
-        
+
         // create a new label for the root bone
         var rootBoneLabel = new Label("Root Bone");
         rootBoneContainer.Add(rootBoneLabel);
-        
+
         // get the root bone value
         var rootBoneValue = _skinnedMeshRenderer.rootBone;
-        
+
         // create a parent VisualElement for the border effect
         var rootBoneFieldBorder = new VisualElement();
         rootBoneFieldBorder.AddToClassList("rootBoneFieldBorder");
         rootBoneContainer.Add(rootBoneFieldBorder);
-        
+
         // create a custom VisualElement that behaves like a disabled ObjectField
         var rootBoneField = new VisualElement();
         rootBoneField.AddToClassList("rootBoneField");
         rootBoneFieldBorder.Add(rootBoneField); // add the rootBoneField to the rootBoneFieldBorder
-        
+
         // create an Image to display the root bone icon
         var rootBoneIcon = new Image();
         rootBoneIcon.image = EditorGUIUtility.ObjectContent(rootBoneValue, typeof(Transform)).image;
         rootBoneIcon.AddToClassList("rootBoneIcon");
         rootBoneField.Add(rootBoneIcon);
-        
+
         // create a label to display the root bone name
         var rootBoneNameLabel = new Label(rootBoneValue != null ? rootBoneValue.name : "None");
         rootBoneField.Add(rootBoneNameLabel);
-        
+
         // handle the MouseDownEvent to select the root bone in the project
         rootBoneField.RegisterCallback<MouseDownEvent>(evt =>
         {
@@ -210,108 +221,92 @@ namespace uk.novavoidhowl.dev.cvrfury.editor.components
             EditorGUIUtility.PingObject(rootBoneValue);
           }
         });
-        
+
         // add the root bone value to the container
         rootBoneContainer.Add(rootBoneFieldBorder);
-        
+
         // add the container to the content
         content.Add(rootBoneContainer);
-        
+        #endregion
+
+        #region Bones list view
         // create a new label
         var bonesLabel = new Label("Bones");
-        
+
         // add the bones label to the content
         content.Add(bonesLabel);
 
+        // create a new container for the bone tree view
+        var boneTreeViewContainer = new VisualElement();
+        boneTreeViewContainer.AddToClassList("container");
 
-        var treeViewState = new UnityEditor.IMGUI.Controls.TreeViewState();
-        var boneTreeView = new BoneTreeView(treeViewState, _skinnedMeshRenderer);
-        boneTreeView.Reload();
+        // create a new tree view
+        var boneTreeView = new BoneTreeView(_skinnedMeshRenderer);
 
-        var boneCount = _skinnedMeshRenderer.bones.Length;
-        var boneHeight = 16; // adjust this value based on the height of each bone item
-        var treeViewHeight = boneCount * boneHeight;
+        // add the tree view to the bone tree view container
+        boneTreeViewContainer.Add(boneTreeView);
 
-        var treeViewContainer = new IMGUIContainer(() =>
-        {
-          boneTreeView.OnGUI(new Rect(0, 0, content.contentRect.width, treeViewHeight));
-        });
-
-        content.Add(treeViewContainer);
-
-        // Set the height of the parent VisualElement
-        content.style.height = treeViewHeight;
-
-
-
+        // add the container to the content
+        content.Add(boneTreeViewContainer);
+        #endregion
       }
-      
     }
   }
-
 
   // supporting class ---------------------------------------
-  public class BoneTreeViewItem : UnityEditor.IMGUI.Controls.TreeViewItem
-  {
-    public Transform Bone { get; set; }
-  }
-  
-  public class BoneTreeView : UnityEditor.IMGUI.Controls.TreeView
+
+  // BoneTreeView class
+  public class BoneTreeView : ScrollView
   {
     private SkinnedMeshRenderer _skinnedMeshRenderer;
-  
-    public BoneTreeView(UnityEditor.IMGUI.Controls.TreeViewState treeViewState, SkinnedMeshRenderer skinnedMeshRenderer) : base(treeViewState)
+
+    public BoneTreeView(SkinnedMeshRenderer skinnedMeshRenderer)
+      : base()
     {
       _skinnedMeshRenderer = skinnedMeshRenderer;
-      Reload(); // Reload the tree to build it and assign the data
-      ExpandAll(); // Expand all items in the tree
-    }
-  
-    protected override UnityEditor.IMGUI.Controls.TreeViewItem BuildRoot()
-    {
-      var root = new BoneTreeViewItem { id = -1, depth = -1, displayName = "Root" };
-    
-      // Create a dictionary to store the TreeViewItems for each bone
-      var boneItems = new Dictionary<Transform, BoneTreeViewItem>();
-    
-      foreach (var bone in _skinnedMeshRenderer.bones)
-      {
-        // If the bone has a parent and the parent is also a bone, set the depth to be one more than the parent's depth
-        // Otherwise, set the depth to 0
-        var depth = bone.parent != null && boneItems.ContainsKey(bone.parent) ? boneItems[bone.parent].depth + 1 : 0;
-    
-        var boneItem = new BoneTreeViewItem { id = bone.GetInstanceID(), depth = depth, displayName = bone.name, Bone = bone };
-        boneItems[bone] = boneItem;
-    
-        // If the bone has a parent and the parent is also a bone, add the boneItem as a child of the parent's TreeViewItem
-        // Otherwise, add the boneItem as a child of the root
-        if (bone.parent != null && boneItems.ContainsKey(bone.parent))
-        {
-          boneItems[bone.parent].AddChild(boneItem);
-        }
-        else
-        {
-          root.AddChild(boneItem);
-        }
-      }
-    
-      return root;
-    }
-  
-    protected override void SelectionChanged(IList<int> selectedIds)
-    {
-      base.SelectionChanged(selectedIds);
-
-      if (selectedIds.Count > 0)
-      {
-        var boneItem = FindItem(selectedIds[0], rootItem) as BoneTreeViewItem;
-        if (boneItem != null)
-        {
-          EditorGUIUtility.PingObject(boneItem.Bone);
-        }
-      }
+      BuildTreeView(_skinnedMeshRenderer.rootBone, 0);
     }
 
+    private Foldout BuildTreeView(Transform bone, int depth)
+    {
+      // Set the text of the Foldout to the bone name
+      var boneFoldout = new Foldout { text = bone.name };
+      boneFoldout.style.marginLeft = depth; // Set the indent level based on the depth
+    
+      // Calculate the color based on the depth
+      int baseColor = 000;
+      int colorValue = Math.Max(0, baseColor + depth * 10);
+      Color32 color32 = new Color32((byte)colorValue, (byte)colorValue, (byte)colorValue, 255);
+      Color color = color32;
+      boneFoldout.style.backgroundColor = new StyleColor(color);    
+
+      // Create a VisualElement to hold the Label and the Button
+      var boneElement = new VisualElement();
+      boneElement.AddToClassList("bone-element"); // Add a class to the VisualElement
+    
+      // Create a Button with the icon and add it to the VisualElement
+      var boneIcon = new Button();
+      var iconTexture = EditorGUIUtility.IconContent("GameObject Icon").image as Texture2D;
+      boneIcon.style.backgroundImage = new StyleBackground(iconTexture); // Use a built-in icon
+      boneIcon.clicked += () => EditorGUIUtility.PingObject(bone.gameObject); // Ping the bone in the hierarchy when the icon is clicked
+      boneIcon.AddToClassList("bone-icon"); // Add a class to the Button
+      boneElement.Add(boneIcon);
+    
+      // Add the VisualElement to the Foldout
+      boneFoldout.Add(boneElement);
+    
+      // Recursively add child bones
+      foreach (Transform child in bone)
+      {
+        var childFoldout = BuildTreeView(child, depth + 1);
+        boneFoldout.Add(childFoldout);
+      }
+    
+      // Add the bone foldout to the tree view
+      this.Add(boneFoldout);
+    
+      return boneFoldout;
+    }
   }
 }
 //#endif
