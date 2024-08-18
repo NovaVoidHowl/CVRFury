4dd31cfc791a5786cf66f0cebd6ca41de7b4ad97
NovaVoidHowl <me@novavoidhowl.com>
2024-08-18 01:39:39 +0100

    feat(GismoAnchor): add runtime components

---
+++ Runtime/CVRFury/Components/CVRFuryGismoAnchor.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using uk.novavoidhowl.dev.cvrfury.supporting_classes.runtime;
+using uk.novavoidhowl.dev.cvrfury.packagecore;
+
+namespace uk.novavoidhowl.dev.cvrfury.runtime
+{
+  [DisallowMultipleComponent]
+  public class CVRFuryGismoAnchor : MonoBehaviour
+  {
+    public GameObject sourceDSUGameObject;
+    public CVRFuryGismo moduleData;
+  }
+}
