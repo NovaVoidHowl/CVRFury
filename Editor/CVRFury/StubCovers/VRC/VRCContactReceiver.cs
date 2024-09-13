f9ae07027133fd7b1bdabdb70eca167ef5484bd7
NovaVoidHowl <me@novavoidhowl.com>
2024-09-13 12:10:40 +0100

    fix(StubCovers): cleanup folder structure

---
+++ Editor/CVRFury/StubCovers/VRC/VRCContactReceiver.cs
@@ -0,0 +1,18 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using uk.novavoidhowl.dev.vrcstub;
+using Constants = uk.novavoidhowl.dev.cvrfury.packagecore.Constants;
+using static uk.novavoidhowl.dev.cvrfury.packagecore.CoreUtils;
+
+namespace uk.novavoidhowl.dev.cvrfury
+{
+  [CustomEditor(typeof(VRCContactReceiver))]
+  public class VRCContactReceiverEditor : VRCGeneralStubEditor {
+    protected override string componentTypeSuffix => "Contact Receiver";
+  }
+}
+#endif
---
+++ Editor/CVRFury/StubCovers/VRC/VRCContactSender.cs
@@ -0,0 +1,18 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using uk.novavoidhowl.dev.vrcstub;
+using Constants = uk.novavoidhowl.dev.cvrfury.packagecore.Constants;
+using static uk.novavoidhowl.dev.cvrfury.packagecore.CoreUtils;
+
+namespace uk.novavoidhowl.dev.cvrfury
+{
+  [CustomEditor(typeof(VRCContactSender))]
+  public class VRCContactSenderEditor : VRCGeneralStubEditor {
+    protected override string componentTypeSuffix => "Contact Sender";
+  }
+}
+#endif
