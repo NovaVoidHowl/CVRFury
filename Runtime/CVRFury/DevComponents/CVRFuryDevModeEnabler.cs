2767c8c07f0706732ce9f5892300768fe158db49
NovaVoidHowl <me@novavoidhowl.com>
2025-02-09 00:02:34 +0000

    feat(DevModeEnabler): fix for Null ref errors when adding enabler after DSU + UI upgrade

--- Runtime/CVRFury/DevComponents/CVRFuryDevModeEnabler.cs
+++ Runtime/CVRFury/DevComponents/CVRFuryDevModeEnabler.cs
@@ -19,6 +19,14 @@ namespace uk.novavoidhowl.dev.cvrfury.runtime
 
     public BoolEvent OnDevModeChanged;
 
+    void Awake()
+    {
+      if (OnDevModeChanged == null)
+      {
+        OnDevModeChanged = new BoolEvent();
+      }
+    }
+
     [SerializeField]
     private bool devModeEnabled;
     public bool DevModeEnabled
