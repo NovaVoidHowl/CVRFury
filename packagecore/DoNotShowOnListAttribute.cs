684d399731001d5dfcc8e9de22e415ff493d81c9
NovaVoidHowl <me@novavoidhowl.com>
2025-02-16 16:56:06 +0000

    feat(AttributeTargets): add DoNotShowOnListAttribute class tag

---
+++ packagecore/DoNotShowOnListAttribute.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace uk.novavoidhowl.dev.cvrfury.packagecore
+{
+  [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+  public class DoNotShowOnListAttribute : Attribute { }
+}
