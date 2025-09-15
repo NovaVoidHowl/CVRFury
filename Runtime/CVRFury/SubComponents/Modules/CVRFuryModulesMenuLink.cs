2effea7b2d0115cbfa9bda834569fc8a2e907aed
NovaVoidHowl <me@novavoidhowl.com>
2025-09-15 18:03:58 +0100

    feat: Update version in CVRFury_VRCContactConverter and add VRC Stub references in ModulesMenuLink and ModulesParametersLink

--- Runtime/CVRFury/SubComponents/Modules/CVRFuryModulesMenuLink.cs
+++ Runtime/CVRFury/SubComponents/Modules/CVRFuryModulesMenuLink.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using uk.novavoidhowl.dev.vrcstub;
 using uk.novavoidhowl.dev.cvrfury.supporting_classes.runtime;
 using uk.novavoidhowl.dev.cvrfury.packagecore;
 using System.Reflection;
+
+// following is provided by the VRC Stub dlls included in the CVRFury project
+using VRC.SDK3.Avatars.ScriptableObjects;
 
 namespace uk.novavoidhowl.dev.cvrfury.runtime
 {
